a3fe26d29b11ca2b7e055828a9a9cb309715d5b3
Kai Kang <kaikang@KaideMacBook-Air.local>
2022-07-15 12:07:33 +0800

    MOD: hide image uploading form

--- src/components/PfpGenerator/PfpGenerator.tsx
+++ src/components/PfpGenerator/PfpGenerator.tsx
@@ -20,6 +20,7 @@ const typeMap = {
 export function PfpGenerator({ address, tokenId, imgUrl }: PfpGeneratorProps) {
     const [fileList, setFileList] = useState<UploadFile[]>([]);
     const [wpfpDataUrl, setWpfpDataUrl] = useState<string>();
+    const [showUploadForm, setShowUploadForm] = useState<boolean>(false);
 
     const generateRawData = (address: string, tokenId: number) => {
         const raw = new BitArray(256);
@@ -92,46 +93,50 @@ export function PfpGenerator({ address, tokenId, imgUrl }: PfpGeneratorProps) {
                 {!wpfpDataUrl && (
                     <p>Please upload your pfp image to generate WNFT pfp.</p>
                 )}
-                {wpfpDataUrl && (
+                {wpfpDataUrl && (<>
                     <AntdImage preview={false} src={wpfpDataUrl}></AntdImage>
+                    <br></br><br></br>
+                    <p>You could also <a href='javascript:void(0)' onClick={() => setShowUploadForm(true)}>use your own image</a> for WNFT PFP</p>
+                </>
                 )}
-
             </Col>
             <Col span={12}>
-                <Form
-                    name="basic"
-                    labelCol={{ span: 4 }}
-                    wrapperCol={{ span: 20 }}
-                    onFinish={onFinish}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                        label="image"
-                        name="image"
-                        rules={[
-                            { required: true, message: 'Please upload your pfp' },
-                            {
-                                validator(rule, value, callback) {
-                                    if (value?.fileList?.length === 0) {
-                                        callback('Please upload your pfp');
-                                        return;
-                                    }
-                                    callback();
-                                }
-                            }
-                        ]}
+                {(showUploadForm || !wpfpDataUrl) && (
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 4 }}
+                        wrapperCol={{ span: 20 }}
+                        onFinish={onFinish}
+                        autoComplete="off"
                     >
-                        <Upload {...props}>
-                            <Button icon={<UploadOutlined />}>Select File</Button>
-                        </Upload>
-                    </Form.Item>
+                        <Form.Item
+                            label="image"
+                            name="image"
+                            rules={[
+                                { required: true, message: 'Please upload your pfp' },
+                                {
+                                    validator(rule, value, callback) {
+                                        if (value?.fileList?.length === 0) {
+                                            callback('Please upload your pfp');
+                                            return;
+                                        }
+                                        callback();
+                                    }
+                                }
+                            ]}
+                        >
+                            <Upload {...props}>
+                                <Button icon={<UploadOutlined />}>Select File</Button>
+                            </Upload>
+                        </Form.Item>
 
-                    <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
-                        <Button type="primary" htmlType="submit">
-                            Generate WNFT PFP
-                        </Button>
-                    </Form.Item>
-                </Form>
+                        <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
+                            <Button type="primary" htmlType="submit">
+                                Generate WNFT PFP
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                )}
             </Col>
         </Row>
     );
